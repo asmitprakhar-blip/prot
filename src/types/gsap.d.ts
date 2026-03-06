0becc040f645fd738bf74e6d4f5cdeeac9a9d90f
Aayan Jha <o2400899@cce.iitmandi.ac.in>
2026-03-07 02:00:41 +0530

    fix: resolution for build errors (gsap types and unused imports)

---
+++ src/types/gsap.d.ts
@@ -0,0 +1,5 @@
+declare module "gsap-trial/*";
+declare module "gsap-trial/ScrollSmoother";
+declare module "gsap-trial/SplitText";
+declare module "gsap-trial/ScrollTrigger";
+declare module "@gsap/react";
